2618152d2214a6633e2487eb53e9b5ae579343aa
Anselmo <digital@mfc.ke>
2021-09-24 14:34:28 +0300

    Blog Added

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7c19abd9036eb7eae5ca9c66802fc71c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ecdc570f-7e03-40c9-a727-7a9d557da2a2'
+};
